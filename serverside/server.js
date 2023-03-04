9221fca6aafe3b76e2010b368030b94eef5ea89e
SumithRaj <sumithraj05@SumithRaj.sr>
2023-03-04 16:22:05 +0530

    updated

--- serverside/server.js
+++ serverside/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {students, faculty} = require('./modules/models')
 
 const port = 8080 || process.env.PORT;
 
@@ -8,21 +9,36 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/clientside`));
 
 app.get('/', async (req, res) => {
+    res.redirect('/students');
+})
+
+// respond from server to client
+app.get('/students', (req,res) => {
     res.status(200).json({
         status: "sucess"
     })
 })
 
-app.get('/student', (req,res) => {
-    res.status(200).json({
-        status: "sucess"
-    })
-})
-
-app.post('/student', async (req,res) => {
-    res.status(200).json({
-        status: "sucess"
-    })
+// from client to server
+app.post('/students', async (req,res) => {
+    console.log(req.body);
+    try{
+        await students.create(req.body).then((data)=>{
+            res.status(201).json({
+                'status': 'sucess',
+                'data':{
+                    'user': {
+                        data
+                    }
+                }
+            }) 
+        });
+    }catch(err){
+        res.status(404).json({
+            'status':'failed',
+            'message':err
+        })
+    }
 })
 
 app.get('/faculty', (req,res) => {
@@ -32,9 +48,24 @@ app.get('/faculty', (req,res) => {
 })
 
 app.post('/faculty', async (req,res) => {
-    res.status(200).json({
-        status: "sucess"
-    })
+    console.log(req.body);
+    try{
+        await faculty.create(req.body).then((data)=>{
+            res.status(201).json({
+                'status': 'sucess',
+                'data':{
+                    'user': {
+                        data
+                    }
+                }
+            }) 
+        });
+    }catch(err){
+        res.status(404).json({
+            'status':'failed',
+            'message':err
+        })
+    }
 })
 
 app.listen(port, (err) => {
