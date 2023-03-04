ddc84984c95e9e81d70c6d341180f66f489d724e
SumithRaj <sumithraj05@SumithRaj.sr>
2023-03-04 14:54:10 +0530

    first commit

---
+++ serverside/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+const port = 8080 || process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/clientside`));
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: "sucess"
+    })
+})
+
+app.get('/student', (req,res) => {
+    res.status(200).json({
+        status: "sucess"
+    })
+})
+
+app.post('/student', (req,res) => {
+    res.status(200).json({
+        status: "sucess"
+    })
+})
+
+app.get('/faculty', (req,res) => {
+    res.status(200).json({
+        status: "sucess"
+    })
+})
+
+app.post('/faculty', (req,res) => {
+    res.status(200).json({
+        status: "sucess"
+    })
+})
+
+app.listen(port, (err) => {
+    if(err) console.log("Server error: ", err);
+    else console.log(`Server listening at port ${port}`);
+})
